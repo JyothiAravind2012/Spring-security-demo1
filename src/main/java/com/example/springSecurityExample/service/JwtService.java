f9a2ed7645d8535b5d24dee1d3ec02d71fff45d2
JyothiAravind2012 <jyothilekshmi32@gmail.com>
2025-12-27 17:44:25 +0530

    comment removed

--- src/main/java/com/example/springSecurityExample/service/JwtService.java
+++ src/main/java/com/example/springSecurityExample/service/JwtService.java
@@ -52,7 +52,6 @@ public class JwtService {
 	}
 
 	  public String extractUserName(String token) {
-	        // extract the username from jwt token
 	        return extractClaim(token, Claims::getSubject);
 	    }
 
